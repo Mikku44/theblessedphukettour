2c42566b450f0dc0d6d13bad620fdc647e8bb444
Mikku44 <104062911+Mikku44@users.noreply.github.com>
2024-11-24 23:38:15 +0700

    fixed deploy fail

---
+++ src/app/login/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return <>
+    <div className="flex justify-center items-center w-full min-h-screen ">
+        <div className="animate-spin">_</div>
+    </div>
+    </>
+}
